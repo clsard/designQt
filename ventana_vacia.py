f7fa1dccc962ca641ac0dc44454b7e9eae83b67b
clsard <clsard@gmail.com>
2023-02-02 13:37:33 +0100

    new file:   ventana_vacia.py

---
+++ ventana_vacia.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+
+class VentanaVacia(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.inicializarUI()
+
+    def inicializarUI(self):
+        self.setGeometry(50, 50, 500,350)
+        self.setWindowTitle("Ventana Vacia")
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ventana = VentanaVacia()
+    sys.exit(app.exec())
