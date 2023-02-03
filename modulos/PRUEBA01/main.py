73447b61132c218b9edbc9cacffd27b1c6d742c6
clsard <clsard@gmail.com>
2023-02-03 17:45:42 +0100

    Probando Qt

---
+++ modulos/PRUEBA01/main.py
@@ -0,0 +1,31 @@
+ 
+from PySide6.QtWidgets import QApplication, QDialog, QTextEdit, QDialogButtonBox
+from ui_dialog import Ui_Dialog
+
+class DialogWindow(QDialog):
+    def __init__(self, parent=None):
+        super(DialogWindow, self).__init__(parent)
+
+        # Crear la interfaz de usuario con Ui_Dialog
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        # Conectar la señal accepted del botón a una función
+        # self.ui.buttonBox.accepted.connect(self.on_accepted)
+        # self.ui.buttonBox.rejected.connect(self.on_rejected)
+        self.ui.pushButton.setText("OKI")
+
+    def on_accepted(self):
+        # hacer algo cuando se pulse el botón OK
+        pass
+
+    def on_rejected(self):
+        # hacer algo cuando se pulse el botón Cancelar
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    dialog = DialogWindow()
+    dialog.show()
+    app.exec_()
---
+++ modulos/PRUEBA01/ui_dialog.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'dialog.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QPushButton, QSizePolicy,
+    QTextEdit, QWidget)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(640, 480)
+        self.textEdit = QTextEdit(Dialog)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(30, 30, 591, 71))
+        self.textEdit_2 = QTextEdit(Dialog)
+        self.textEdit_2.setObjectName(u"textEdit_2")
+        self.textEdit_2.setGeometry(QRect(30, 130, 591, 71))
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(390, 430, 80, 27))
+        self.pushButton_2 = QPushButton(Dialog)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(510, 430, 80, 27))
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"Ok", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Dialog", u"Cancel", None))
+    # retranslateUi
+
