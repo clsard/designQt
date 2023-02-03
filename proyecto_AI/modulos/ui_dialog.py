265f96deaad090c69d63b1a8fd4217b22a6f366a
clsard <clsard@gmail.com>
2023-02-03 19:02:35 +0100

    Reestructuracion

--- proyecto_AI/modulos/ui_dialog.py
+++ proyecto_AI/modulos/ui_dialog.py
@@ -15,8 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDialog, QPushButton, QSizePolicy,
-    QTextEdit, QWidget)
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QPushButton,
+    QSizePolicy, QTextEdit, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -25,16 +25,22 @@ class Ui_Dialog(object):
         Dialog.resize(640, 480)
         self.textEdit = QTextEdit(Dialog)
         self.textEdit.setObjectName(u"textEdit")
-        self.textEdit.setGeometry(QRect(30, 30, 591, 71))
+        self.textEdit.setGeometry(QRect(30, 75, 591, 71))
         self.textEdit_2 = QTextEdit(Dialog)
         self.textEdit_2.setObjectName(u"textEdit_2")
-        self.textEdit_2.setGeometry(QRect(30, 130, 591, 71))
+        self.textEdit_2.setGeometry(QRect(30, 190, 591, 71))
         self.pushButton = QPushButton(Dialog)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(390, 430, 80, 27))
+        self.pushButton.setGeometry(QRect(30, 430, 111, 27))
         self.pushButton_2 = QPushButton(Dialog)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(510, 430, 80, 27))
+        self.pushButton_2.setGeometry(QRect(510, 430, 111, 27))
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(30, 50, 56, 19))
+        self.label_2 = QLabel(Dialog)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(30, 170, 56, 19))
 
         self.retranslateUi(Dialog)
 
@@ -43,7 +49,9 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
-        self.pushButton.setText(QCoreApplication.translate("Dialog", u"Ok", None))
-        self.pushButton_2.setText(QCoreApplication.translate("Dialog", u"Cancel", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"Borrar contenido", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Dialog", u"Solicitar imagen", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"Espa\u00f1ol", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"Ingl\u00e9s", None))
     # retranslateUi
 
